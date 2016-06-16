0769e14fcd2e93577a5e3c20792cbb6db2eed769
Jordan Ramirez <jordan.ramirez@student.hu.nl>
2016-06-16 22:23:09 +0200

    Added header in files, removed some unccesary lines

--- Source/src/VideoTag.cpp
+++ Source/src/VideoTag.cpp
@@ -1,4 +1,53 @@
-#include "../Include/VideoTag.hpp"
+//! \addtogroup <Project_Number> <Project_name>
+//! \brief Apriltag recognition 
+//!
+//! Module to recognise AprilTags 
+//! 
+//! \file   VideoTag.cpp
+//! \author Ole Agterberg, 1651981
+//! \author Daniel Klomp, 1661521
+//! \date   Created: 9-6-2016
+//! \date   Last Modified: 16-6-2016
+//! \brief  Functional part to detect tags and match them to a config file.
+//!
+//! Defenitions of all functions for use in VideoTag.cpp
+//!
+//! More info on this system can be found on the wiki page:
+//! https://roborescue.nl/index.php/Position_from_Video_using_Tags
+//!
+//! \copyright Copyright © 2016, HU University of Applied Sciences Utrecht. 
+//! All rights reserved.
+//! 
+//! License: newBSD
+//!
+//! Redistribution and use in source and binary forms, 
+//! with or without modification, are permitted provided that 
+//! the following conditions are met:
+//! - Redistributions of source code must retain the above copyright notice, 
+//!   this list of conditions and the following disclaimer.
+//! - Redistributions in binary form must reproduce the above copyright notice, 
+//!   this list of conditions and the following disclaimer in the documentation 
+//!   and/or other materials provided with the distribution.
+//! - Neither the name of the HU University of Applied Sciences Utrecht 
+//!   nor the names of its contributors may be used to endorse or promote 
+//!   products derived from this software without specific prior written 
+//!   permission.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+//! BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+//! AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+//! IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+//! BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+//! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+//! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+//! WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+//! OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+//! EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ~< HEADER_VERSION 2016 04 12 >~
+
+#include "../include/VideoTag.hpp"
 
 VideoTag::VideoTag(double factor):
 	Sensor{ factor },
@@ -321,14 +370,9 @@ void VideoTag::wRo_to_euler( Eigen::Matrix3d& wRo, double& yaw, double& pitch, d
 
 // here is were everything begins
 int main(int argc, char* argv[]) {
-	int a = 1;
-	LockingSharedObject<int> test(a);
-	SharedObject<int>::Accessor acc(test);
-	cout << acc.access() << "LALALALALALAL";
 	
 	VideoTag tag (0.0);
     	
-	
 	while(true){
 	  tag.get_data(); 
 	}
